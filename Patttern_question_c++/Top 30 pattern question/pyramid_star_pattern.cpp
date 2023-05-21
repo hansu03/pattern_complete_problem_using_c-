7981ac58035309a88bad4425ba5fc177ba0c9663
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:24:30 +0530

    Update pyramid_star_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/pyramid_star_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/pyramid_star_pattern.cpp
@@ -1,10 +1,23 @@
-#include<iostream>
+// wap for pyramid star pattern 
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    // print spaces
+    for (int j = 0; j < size - i - 1; j++) {
+      cout << " ";
+    }
+    // print stars
+    for (int k = 0; k < 2 * i + 1; k++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
+
+// humne kya kiya 
+// we have first
