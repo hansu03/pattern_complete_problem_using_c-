4b1b21e7fc6d325fbd18bb3e8ab54fd79ff8d243
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:33:19 +0530

    Update left_triangle_number_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/left_triangle_number_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/left_triangle_number_pattern.cpp
@@ -1,10 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 
-
-    return 0;
-
+  int size = 5;
+  // loop to print the pattern
+  for (int i = 0; i < size; i++) {
+    // print column
+    for (int j = 0; j <= i; j++) {
+      cout << (j+1);
+    }
+    cout << "\n";
+  }
+  return 0;
 }
