3134b026f043ceb3dfcb845d8eda563c786de05b
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:16:40 +0530

    Update left_triangle_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/left_triangle_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/left_triangle_pattern.cpp
@@ -1,10 +1,22 @@
-#include<iostream>
+// left triangle star pattern
+#include <iostream>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+    
+  // size of the triangle
+  int size = 5;
+    
+  // loop to print the pattern  begins here
+  for (int i = 0; i < size; i++) {
+    // print column 
+    for (int j = 0; j <= i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
+
+
+// output will return left star pattern
