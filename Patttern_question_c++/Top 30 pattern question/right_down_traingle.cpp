6bcb0902204915929e1349ca3f0a50cecd82a2b4
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:21:00 +0530

    Update right_down_traingle.cpp

--- Patttern_question_c++/Top 30 pattern question/right_down_traingle.cpp
+++ Patttern_question_c++/Top 30 pattern question/right_down_traingle.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
+// right down triangle
+#include <iostream>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+  // size of the triangle
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    // print spaces
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    // print stars
+    for (int j = size; j > i; j--) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
+
+
+//output
+// provide output as right down triangle 
