95158deb3770cf3d7397d79c8a2c0b44570a0f8d
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:30:02 +0530

    Update hollow_number_diamond_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/hollow_number_diamond_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/hollow_number_diamond_pattern.cpp
@@ -1,10 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+  int size = 5, num = 1;
+  // upside pyramid
+  for (int i = 1; i <= size; i++) {
+    // printing spaces
+    for (int j = size; j > i; j--) {
+      cout << " ";
+    }
+    // printing number
+    for (int k = 0; k < i * 2 - 1; k++) {
+      if (k == 0 || k == 2 * i - 2) {
+        cout << num++;
+      } else {
+        cout << " ";
+      }
+    }
+    // set the number to 1
+    num = 1;
+    cout << "\n";
+  }
+  // downside triangle
+  for (int i = 1; i < size; i++) {
+    // printing spaces
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    // printing number
+    for (int k = (size - i) * 2 - 1; k >= 1; k--) {
+      if (k == 1 || k == (size - i) * 2 - 1) {
+        cout << num++;
+      } else {
+        cout << " ";
+      }
+    }
+    // set the number to 1
+    num = 1;
+    cout << "\n";
+  }
+  return 0;
 }
