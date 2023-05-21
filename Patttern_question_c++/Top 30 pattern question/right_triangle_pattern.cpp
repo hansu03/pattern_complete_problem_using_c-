78345e2fff1435cb6a2f6f11517f51077167e701
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:18:30 +0530

    Update right_triangle_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/right_triangle_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/right_triangle_pattern.cpp
@@ -1,10 +1,28 @@
-#include<iostream>
+//right triangle pattern 
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
 
 
-    return 0;
-
+int main() {
+  // size of the triangle
+  int size = 5;
+  // loop to print the pattern
+  for (int i = 0; i < size; i++) {
+    // print spaces
+    for (int j = 1; j < size - i; j++) {
+      cout << " ";
+    }
+    // print stars
+    for (int k = 0; k <= i; k++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
+
+
+//output
+// return right triangle pattern
