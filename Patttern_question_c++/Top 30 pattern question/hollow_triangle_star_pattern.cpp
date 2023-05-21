63d72d41eff0f3b44afbaab34d839f1667f1f5d9
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:22:44 +0530

    Update hollow_triangle_star_pattern.cpp

--- Patttern_question_c++/Top 30 pattern question/hollow_triangle_star_pattern.cpp
+++ Patttern_question_c++/Top 30 pattern question/hollow_triangle_star_pattern.cpp
@@ -1,10 +1,26 @@
-#include<iostream>
+// wap to print a hollow triangle star program
+#include <iostream>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+  // size of the triangle
+  int size = 5;
+  for (int i = 1; i <= size; i++) {
+    for (int j = 0; j < i; j++) {
+      // not last row
+      if (i != size) {
+        if (j == 0 || j == i - 1) {
+          cout << "*";
+        } else {
+          cout << " ";
+        }
+      }
+      // last row
+      else {
+        cout << "*";
+      }
+    }
+    cout << "\n";
+  }
+  return 0;
 }
