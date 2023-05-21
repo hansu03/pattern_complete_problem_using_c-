0a8b6e0a71b123650aeb9056ecea15cf533930a0
hansu03 <114220372+hansu03@users.noreply.github.com>
2023-05-21 20:19:55 +0530

    Update left_down_triangle.cpp

--- Patttern_question_c++/Top 30 pattern question/left_down_triangle.cpp
+++ Patttern_question_c++/Top 30 pattern question/left_down_triangle.cpp
@@ -1,10 +1,20 @@
-#include<iostream>
+// program to print left down triangle
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-
-
-    return 0;
-
+int main() {
+  // size of the triangle
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    // print stars
+    for (int j = 0; j < size - i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }
+
+// output 
+// gives left down triangle
